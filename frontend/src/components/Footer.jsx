9d1562353bbc664790cc160a524b8f100ef97e56
Muhammad Arslan Wajid <167841484+marslanwajid@users.noreply.github.com>
2024-05-25 12:00:06 +0500

    Add files via upload

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+export default function Footer() {
+    
+    let stylee = {
+        backgroundColor: "grey",
+        height:"10vh"
+        
+    }
+    return (
+        <>
+        <div style={stylee} className=""></div>
+        </>
+       
+    )
+}
